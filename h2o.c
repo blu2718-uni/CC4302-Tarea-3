7ff4e4b18216a167975378de18bbb68f2c53b470
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-04-26 21:46:50 -0400

    fear(h2o): assignment finished

--- h2o.c
+++ h2o.c
@@ -1,27 +1,96 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <pthread.h>
 
 #include "pss.h"
 #include "h2o.h"
 
-// Defina aca nuevos tipos, variables globales, otras funciones que
-// necesite, etc.
+pthread_mutex_t m;
+Queue *oq, *hq;
+
+typedef struct { 
+  int ready;
+  H2O *molecule;
+  Oxygen *atom;
+  pthread_cond_t c;
+} oxyRequest;
+
+typedef struct {
+  int ready;
+  H2O *molecule;
+  Hydrogen *atom;
+  pthread_cond_t c;
+} hdgRequest;
 
 void initH2O(void) {
-  ... complete ...
+  pthread_mutex_init(&m, NULL);
+  oq = makeQueue();
+  hq = makeQueue();
 }
 
 void endH2O(void) {
-  ... complete ...
+  pthread_mutex_destroy(&m);
+  destroyQueue(oq);
+  destroyQueue(hq);
 }
 
 H2O *combineOxy(Oxygen *o) {
-  ... complete ...
+  H2O *result;
+  pthread_mutex_lock(&m);
+  if (queueLength(hq) > 1) {
+    hdgRequest *h1 = get(hq);
+    hdgRequest *h2 = get(hq);
+
+    h1->ready = 1; h2->ready = 1;
+    
+    result = makeH2O(h1->atom, h2->atom, o);
+    
+    h1->molecule = result;
+    h2->molecule = result;
+    
+    pthread_cond_signal(&h1->c);
+    pthread_cond_signal(&h2->c);
+  } else {
+    oxyRequest req = {0, NULL, o, PTHREAD_COND_INITIALIZER};
+
+    put(oq, &req);
+    
+    while (!req.ready) {
+      pthread_cond_wait(&req.c, &m);
+    }
+    
+    result = req.molecule;
+  }
+  pthread_mutex_unlock(&m);
+  return result;
 }
 
 H2O *combineHydro(Hydrogen *h) {
-  ... complete ...
+  H2O *result;
+  pthread_mutex_lock(&m);
+  if (queueLength(oq) > 0 && queueLength(hq) > 0) {
+    hdgRequest *h1 = get(hq);
+    oxyRequest *o1 = get(oq);
+
+    h1->ready = 1; o1->ready = 1;
+
+    result = makeH2O(h1->atom, h, o1->atom);
+
+    h1->molecule = result;
+    o1->molecule = result;
+
+    pthread_cond_signal(&h1->c);
+    pthread_cond_signal(&o1->c);
+  } else {
+    hdgRequest req = {0, NULL, h, PTHREAD_COND_INITIALIZER};
+    
+    put(hq, &req);
+
+    while (!req.ready) {
+      pthread_cond_wait(&req.c, &m);
+    }
+
+    result = req.molecule;
+  }
+  pthread_mutex_unlock(&m);
+  return result;
 }
